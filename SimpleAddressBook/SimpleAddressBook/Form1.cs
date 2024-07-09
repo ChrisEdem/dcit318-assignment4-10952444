41507aa9f7749a7c8c1bb30e799d0c4f38749d54
ChrisEdem <102974043+ChrisEdem@users.noreply.github.com>
2024-07-09 15:33:03 +0000

    Add files via upload

---
+++ SimpleAddressBook/SimpleAddressBook/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace AddressBookApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string phone = txtPhone.Text;
+
+            
+          
+
+            MessageBox.Show("Information saved successfully!");
+        }
+    }
+}
